67859e16e729ce5097d5cc9b3cb15dd6e896c28d
ritabc <ritabc@gmail.com>
2019-02-03 12:22:26 -0500

    Add failing test for Paragraph#word_count_per_sentence

--- spec/models/paragraph_spec.rb
+++ spec/models/paragraph_spec.rb
@@ -18,4 +18,11 @@ describe Paragraph do
       expect(@test_paragraph2.split_into_sentences).to eq(["Lorem Ipsum.", "Oh Latin!", "But is it really Latin?"])
     end
   end
+
+  describe '#word_count_per_sentence' do
+    it 'creates a sentences hash with a sentence : word_count pair' do
+      expect(@test_paragraph1.word_count_per_sentence).to eq({"Lorem Ipsum is simply dummy text of the printing and typesetting industry." => 12, "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." => 31, "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged." => 17})
+      expect(@test_paragraph2.word_count_per_sentence).to eq({"Lorem Ipsum." => 2, "Oh Latin!" => 2, "But is it really Latin?" => 5})
+    end
+  end
 end
