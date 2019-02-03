8f135f0f26b218bf3096360619686c049d72f8a8
ritabc <ritabc@gmail.com>
2019-02-03 12:06:45 -0500

    Build out Paragraph#split_into_sentences and pass corresponding test

--- app/models/paragraph.rb
+++ app/models/paragraph.rb
@@ -1,7 +1,17 @@
 class Paragraph
-  attr_writer(:content)
+  attr_accessor(:content)
 
   def initialize(attributes)
     @content = attributes.fetch(:paragraph_content)
+  end
+
+  def split_into_sentences
+    # substitute every sentence ender (period, question mark, or exclamation) with
+    # itself plus a double comma, then split at the double commas.
+    # Finally, strip the leading whitespace from each sentence
+    @sentences = self.content.gsub(/[.?!]/, '\0,,').split(',,').map do |sentence|
+      sentence.lstrip
+    end
+    @sentences
   end
 end
