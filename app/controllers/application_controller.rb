f76f6f27c64febec37aed9529ebe87882e9d3e72
ritabc <ritabc@gmail.com>
2019-02-04 10:30:44 -0500

    Add exception handling for excluded value param

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  include Response
+
+  rescue_from NoMethodError do |exception|
+    json_response({ Error: "A param named 'value' is required" }, :unprocessable_entity)
+  end
+
 end
